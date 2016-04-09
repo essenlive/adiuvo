f116be3eabbdba51565a1cc415314ce46fd3dbc8
essenlive <perchais.quentin@gmail.com>
2016-04-09 17:17:59 +0200

    minor changes

--- client/templates/widgets_components/e_mirrors/e_mirrors.js
+++ client/templates/widgets_components/e_mirrors/e_mirrors.js
@@ -1,5 +1,10 @@
 Template.eMirrors.helpers({
-  wEmirrorsDisplay: function(){
-    return Meteor.user().profile && Meteor.user().profile.status.wEmirrorsDisplay;
-  },
+    wEmirrorsDisplay: function(){
+        return Meteor.user().profile && Meteor.user().profile.status.wEmirrorsDisplay;
+    },
+    wEmirrorsActive: function(){
+        var active = "inactive"
+        if(Meteor.user().profile && Meteor.user().profile.status.wEmirrorsActive){ active = "active" }
+        return active;
+    },
 })
