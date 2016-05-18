43ad193dfe586509a245e40755ebfd55e1d2ef35
essenlive <perchais.quentin@gmail.com>
2016-05-18 15:33:06 -0700

    minor changes

--- client/templates/components/footages/footages.js
+++ client/templates/components/footages/footages.js
@@ -1,25 +1,35 @@
 Template.frontFootage.onRendered(function () {
-  videojs("front-footage-vid").ready(function(){
-    frontFootagePlayer = this;
+	videojs("front-footage-vid").ready(function(){
+		frontFootagePlayer = this;
 
-    Tracker.autorun(function fControlToggle() {
-	  	var state = State.findOne({name: 'state'}).status;
-      var status = state && state.fStatus; 
-      if ( status ) { frontFootagePlayer.pause(); }
-      else { frontFootagePlayer.play(); }
-    });
+		Tracker.autorun(function() {
+			var state = State.findOne({name: 'state'}).status;
+			var status = state && state.fStatus;
+
+			if ( status ) { 
+				frontFootagePlayer.pause();
+				$("#front-footage").css({border: "1rem solid red"});
+				console.log("play");
+			}
+			else { 
+				frontFootagePlayer.play();
+				$("#front-footage").css({border: "1rem solid green"});
+				console.log("play");
+			}
+
+		});
 
     // Tracker.autorun(function fControlGoto() {
     //   var goTo = Meteor.user().profile.status.fCurrentTime;
     //   frontFootagePlayer.currentTime(goTo);
     // });
 
-  });
+});
 });
 
 Template.rearFootage.onRendered(function () {
-  videojs("rear-footage-vid").ready(function(){
-    rearFootagePlayer = this;
+	videojs("rear-footage-vid").ready(function(){
+		rearFootagePlayer = this;
 
     // Tracker.autorun(function fControlToggle() {
     //   var status = Meteor.user().profile.status.fStatus;
@@ -32,11 +42,11 @@ Template.rearFootage.onRendered(function () {
     //   rearFootagePlayer.currentTime(goTo);
     // });
 
-  });
+});
 });
 Template.leftFootage.onRendered(function () {
-  videojs("left-footage-vid").ready(function(){
-    leftFootagePlayer = this;
+	videojs("left-footage-vid").ready(function(){
+		leftFootagePlayer = this;
 
     // Tracker.autorun(function fControlToggle() {
     //   var status = Meteor.user().profile.status.fStatus;
@@ -49,11 +59,11 @@ Template.leftFootage.onRendered(function () {
     //   leftFootagePlayer.currentTime(goTo);
     // });
 
-  });
+});
 });
 Template.rightFootage.onRendered(function () {
-  videojs("right-footage-vid").ready(function(){
-    rightFootagePlayer = this;
+	videojs("right-footage-vid").ready(function(){
+		rightFootagePlayer = this;
 
     // Tracker.autorun(function fControlToggle() {
     //   var status = Meteor.user().profile.status.fStatus;
@@ -66,47 +76,46 @@ Template.rightFootage.onRendered(function () {
     //   rightFootagePlayer.currentTime(goTo);
     // });
 
-  });
+});
 });
 
-
 Template.frontFootage.helpers({
-  route: function(){
-    return Router.current().route.getName();
-  },
-  footageScenario: function(){
+	route: function(){
+		return Router.current().route.getName();
+	},
+	footageScenario: function(){
 
-    var state = State.findOne({name: 'state'});
-    var scenario = state && state.status.fScenario;
-    var scenarioSource = '<video id="front-footage-vid" autoplay loop class="video-js" preload="auto"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Front_View_0'+ scenario +'.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser. </video>'
-    return scenarioSource;
+		var state = State.findOne({name: 'state'});
+		var scenario = state && state.status.fScenario;
+		var scenarioSource = '<video id="front-footage-vid" autoplay loop class="video-js" preload="auto" poster="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/driving_footage.png"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Front_View_0'+ scenario +'.webm" type="video/webm" /><source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Front_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		return scenarioSource;
 
-  },
+	},
 })
 
 Template.rearFootage.helpers({
-  footageScenario: function(){
-    var state = State.findOne({name: 'state'});
-    var scenario = state && state.status.fScenario;
-    var scenarioSource = '<video id="rear-footage-vid" autoplay loop class="video-js" preload="auto"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Rear_View_0'+ scenario +'.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser. </video>'
-    return scenarioSource;
-  },
+	footageScenario: function(){
+		var state = State.findOne({name: 'state'});
+		var scenario = state && state.status.fScenario;
+		var scenarioSource = '<video id="rear-footage-vid" autoplay loop class="video-js" preload="auto" poster="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/driving_footage.png"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Rear_View_0'+ scenario +'.webm" type="video/webm" /><source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Rear_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		return scenarioSource;
+	},
 })
 
 Template.leftFootage.helpers({
-  footageScenario: function(){
-    var state = State.findOne({name: 'state'});
-    var scenario = state && state.status.fScenario;
-    var scenarioSource = '<video id="left-footage-vid" autoplay loop class="video-js" preload="auto"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Left_View_0'+ scenario +'.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser. </video>'
-    return scenarioSource;
-  },
+	footageScenario: function(){
+		var state = State.findOne({name: 'state'});
+		var scenario = state && state.status.fScenario;
+		var scenarioSource = '<video id="left-footage-vid" autoplay loop class="video-js" preload="auto" poster="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/driving_footage.png"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Left_View_0'+ scenario +'.webm" type="video/webm" /><source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Left_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		return scenarioSource;
+	},
 })
 
 Template.rightFootage.helpers({
-  footageScenario: function(){
-    var state = State.findOne({name: 'state'});
-    var scenario = state && state.status.fScenario;
-    var scenarioSource = '<video id="right-footage-vid" autoplay loop class="video-js" preload="auto"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Right_View_0'+ scenario +'.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser. </video>'
-    return scenarioSource;
-  },
+	footageScenario: function(){
+		var state = State.findOne({name: 'state'});
+		var scenario = state && state.status.fScenario;
+		var scenarioSource = '<video id="right-footage-vid" autoplay loop class="video-js" preload="auto" poster="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/driving_footage.png"> <source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Right_View_0'+ scenario +'.webm" type="video/webm" /><source src="https://s3.eu-central-1.amazonaws.com/adiuvobucket/video/Valeo_Footage_Right_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		return scenarioSource;
+	},
 })
--- server/serial.js
+++ server/serial.js
@@ -1,30 +1,30 @@
 Meteor.startup(function() {
   // try{
-  serialPort = new SerialPort.SerialPort('/COM3', {
-  baudrate: 38400,
-  parser: SerialPort.parsers.readline('\r\n')
-  }); 
+  // serialPort = new SerialPort.SerialPort('/COM3', {
+  // baudrate: 38400,
+  // parser: SerialPort.parsers.readline('\r\n')
+  // }); 
   // }
   // catch(e){
   //  console.log(e)
   // }
 
-  serialPort.on('open', function() {
-  console.log('Port open');
-  });
+  // serialPort.on('open', function() {
+  // console.log('Port open');
+  // });
 
-  serialPort.on('data', Meteor.bindEnvironment(function (error, result) {
-  var data = error;
-  console.log("answer ----------------");
-  console.log(data);
-  }));
+  // serialPort.on('data', Meteor.bindEnvironment(function (error, result) {
+  // var data = error;
+  // console.log("answer ----------------");
+  // console.log(data);
+  // }));
 
 });
 
 Meteor.methods({
   sendToSerialPort: function (message) {
-  serialPort.write(message);
+  // serialPort.write(message);
   console.log(message);
   return message;
-  },
+},
 }); 
