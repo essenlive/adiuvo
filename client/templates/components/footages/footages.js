871dddd2c03e28ded917ea7fcb98006be84dc8c4
essenlive <perchais.quentin@gmail.com>
2016-05-24 19:34:11 -0700

    improved video playback

--- client/templates/components/footages/footages.js
+++ client/templates/components/footages/footages.js
@@ -1,9 +1,9 @@
 Template.frontFootage.onRendered(function () {
 
 	Tracker.autorun(function() {
-		var status = State.findOne({name: 'state'}).status.fstatus;
-		if (status) {
-
+		var state = State.findOne({name: 'state'});
+		if (state) {
+			var status = state && state.status.fStatus;
 			if ( status === 1 ) { 
 				$("#front-footage-vid")[0].pause();
 				$("#front-footage").css({border: "1rem solid red"});
@@ -104,8 +104,8 @@ Template.frontFootage.helpers({
 
 		var state = State.findOne({name: 'state'});
 		var scenario = state && state.status.fScenario;
-		if (!scenario) var scenarioSource = '<video id="front-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Front_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Front_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
-		else var scenarioSource = '<video id="front-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Front_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Front_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		if (!scenario) var scenarioSource = '<video id="front-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Front_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Front_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
+		else var scenarioSource = '<video id="front-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Front_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Front_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
 			return scenarioSource;
 
 	},
@@ -115,8 +115,8 @@ Template.rearFootage.helpers({
 	footageScenario: function(){
 		var state = State.findOne({name: 'state'});
 		var scenario = state && state.status.fScenario;
-		if (!scenario) var scenarioSource = '<video id="rear-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Rear_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Rear_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
-		else var scenarioSource = '<video id="rear-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Rear_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Rear_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		if (!scenario) var scenarioSource = '<video id="rear-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Rear_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Rear_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
+		else var scenarioSource = '<video id="rear-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Rear_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Rear_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
 			return scenarioSource;
 	},
 })
@@ -125,8 +125,8 @@ Template.leftFootage.helpers({
 	footageScenario: function(){
 		var state = State.findOne({name: 'state'});
 		var scenario = state && state.status.fScenario;
-		if (!scenario) var scenarioSource = '<video id="left-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Left_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Left_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
-		else var scenarioSource = '<video id="left-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Left_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Left_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		if (!scenario) var scenarioSource = '<video id="left-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Left_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Left_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
+		else var scenarioSource = '<video id="left-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Left_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Left_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
 			return scenarioSource;
 	},
 })
@@ -135,8 +135,8 @@ Template.rightFootage.helpers({
 	footageScenario: function(){
 		var state = State.findOne({name: 'state'});
 		var scenario = state && state.status.fScenario;
-		if (!scenario) var scenarioSource = '<video id="right-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Right_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Right_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
-		else var scenarioSource = '<video id="right-footage-vid" autoplay loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Right_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Right_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
+		if (!scenario) var scenarioSource = '<video id="right-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Right_View_04.webm" type="video/webm" /><source src="/video/Valeo_Footage_Right_View_04.mp4" type="video/mp4" />Your browser does not support the video tag.</video>';
+		else var scenarioSource = '<video id="right-footage-vid" loop preload="auto" poster="/video/driving_footage.png"> <source src="/video/Valeo_Footage_Right_View_0'+ scenario +'.webm" type="video/webm" /><source src="/video/Valeo_Footage_Right_View_0'+ scenario +'.mp4" type="video/mp4" />Your browser does not support the video tag.</video>'
 			return scenarioSource;
 	},
 })
--- server/serial.js
+++ server/serial.js
@@ -1,29 +1,24 @@
-Meteor.startup(function() {
-	try{
-		serialPort = new SerialPort.SerialPort('/COM3', {
-			baudrate: 4800,
-			parser: SerialPort.parsers.readline('\r\n')
-		}); 
-	}
-	catch(e){
-		console.log(e)
-	}
+// Meteor.startup(function() {
+// 	serialPort = new SerialPort.SerialPort('/COM3', {
+// 		baudrate: 4800,
+// 		parser: SerialPort.parsers.readline('\r\n')
+// 	}); 
 
-	serialPort.on('open', function() {
-		console.log('Port open');
-	});
+// 	serialPort.on('open', function() {
+// 		console.log('Port open');
+// 	});
 
-	serialPort.on('data', Meteor.bindEnvironment(function (error, result) {
-		var data = error;
-		console.log("answer" + data);		
-	}));
+// 	serialPort.on('data', Meteor.bindEnvironment(function (error, result) {
+// 		var data = error;
+// 		console.log("answer" + data);		
+// 	}));
 
-});
+// });
 
-Meteor.methods({
-	sendToSerialPort: function (message) {
-		serialPort.write(message);
-		console.log(message);
-		return message;
-	},
-});  
+// Meteor.methods({
+// 	sendToSerialPort: function (message) {
+// 		serialPort.write(message);
+// 		console.log(message);
+// 		return message;
+// 	},
+// });  
